ffa61e73728be94a23ee268c87ee325758ff9bbb
Jacob Tardieu <jacob@tardieu.net>
2017-08-06 21:01:29 +0200

    Remove `.gif` in the name and add print

--- indexer.py
+++ indexer.py
@@ -13,11 +13,10 @@ es.indices.create(index="gifs", body=json_index, ignore=400)
 
 gifs_names = map(os.path.basename, glob.glob("example_gifs/*.gif"))
 
-for name in gifs_names:
-    es.index(index="gifs", doc_type='gif', body={'timestamp': datetime.now(), 'name': name}, id = name)
-
-#res = es.index(index="gifs", doc_type='gif', body=doc, id = doc["name"])
-#print(res['created'])
+for complete_name in gifs_names:
+    name = complete_name[:-4] # Remove the `.gif` part in the name
+    res = es.index(index="gifs", doc_type='gif', body={'timestamp': datetime.now(), 'name': name}, id = name)
+    print("Indexed %s and created: %s" % (complete_name, res['created']))
 
 es.indices.refresh(index="gifs")
 
